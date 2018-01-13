392913efa8ceddef1edae89d419ebde1612f37e3
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2018-01-13 23:45:14 +0600

    Entity Layer add

---
+++ IslahGroup.DotNet.EntityLayer/Member.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IslahGroup.DotNet.EntityLayer
+{
+    public class Class1
+    {
+    }
+}
