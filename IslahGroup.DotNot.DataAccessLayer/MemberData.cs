332158a98703dc9abc6146574eb53db367208e48
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2018-01-13 22:55:56 +0600

    Data Access Layer add

--- IslahGroup.DotNot.DataAccessLayer/MemberData.cs
+++ IslahGroup.DotNot.DataAccessLayer/MemberData.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IslahGroup.DotNet.BusinessLogicLayer
+namespace IslahGroup.DotNot.DataAccessLayer
 {
-    public class MemberLogic
+    public class MemberData
     {
     }
 }
