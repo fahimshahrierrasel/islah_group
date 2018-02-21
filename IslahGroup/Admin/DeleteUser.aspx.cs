c874e35dc244030187f8ad6576524f5639f8c146
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2018-02-21 22:34:09 +0600

    User Delete add

---
+++ IslahGroup/Admin/DeleteUser.aspx.cs
@@ -0,0 +1,26 @@
+﻿using IslahGroup.DotNet.BusinessLogicLayer;
+using System;
+using System.Collections.Generic;
+
+namespace IslahGroup.Admin
+{
+    public partial class DeleteUser : System.Web.UI.Page
+    {
+        string userId;
+        IGUserLogic iGUserLogic;
+        public DeleteUser()
+        {
+            iGUserLogic = new IGUserLogic();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            userId = Request.QueryString["UID"].ToString();
+            Dictionary<string, string> user = new Dictionary<string, string>()
+            {
+                { "UserId", userId }
+            };
+            iGUserLogic.DeleteIGUser(user);
+            Response.Redirect("Management.aspx");
+        }
+    }
+}
