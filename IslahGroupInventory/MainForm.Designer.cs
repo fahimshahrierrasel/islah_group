e9199893d45dbee253d67c8be5d1a894264667c0
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2018-03-20 01:50:28 +0600

    UsersControl add

--- IslahGroupInventory/MainForm.Designer.cs
+++ IslahGroupInventory/MainForm.Designer.cs
@@ -429,25 +429,9 @@
             this.label98 = new System.Windows.Forms.Label();
             this.label99 = new System.Windows.Forms.Label();
             this.tabPageUser = new System.Windows.Forms.TabPage();
-            this.groupBox29 = new System.Windows.Forms.GroupBox();
-            this.label88 = new System.Windows.Forms.Label();
-            this.gridControlUsers = new DevExpress.XtraGrid.GridControl();
-            this.gridViewUsers = new DevExpress.XtraGrid.Views.Grid.GridView();
-            this.groupBox30 = new System.Windows.Forms.GroupBox();
-            this.textBox24 = new System.Windows.Forms.TextBox();
-            this.label96 = new System.Windows.Forms.Label();
-            this.textBox23 = new System.Windows.Forms.TextBox();
-            this.label95 = new System.Windows.Forms.Label();
-            this.textBox21 = new System.Windows.Forms.TextBox();
-            this.label93 = new System.Windows.Forms.Label();
-            this.button15 = new System.Windows.Forms.Button();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.textBox19 = new System.Windows.Forms.TextBox();
-            this.label89 = new System.Windows.Forms.Label();
-            this.textBox20 = new System.Windows.Forms.TextBox();
-            this.label91 = new System.Windows.Forms.Label();
             this.imageListTab = new System.Windows.Forms.ImageList(this.components);
             this.bindingSourcePurchaseItem = new System.Windows.Forms.BindingSource(this.components);
+            this.usersControl1 = new IslahGroupInventory.ViewControls.UsersControl();
             this.menuStrip1.SuspendLayout();
             this.statusStrip1.SuspendLayout();
             this.tabControlMain.SuspendLayout();
@@ -538,10 +522,6 @@
             ((System.ComponentModel.ISupportInitialize)(this.gridViewWorks)).BeginInit();
             this.groupBox32.SuspendLayout();
             this.tabPageUser.SuspendLayout();
-            this.groupBox29.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.gridControlUsers)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.gridViewUsers)).BeginInit();
-            this.groupBox30.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSourcePurchaseItem)).BeginInit();
             this.SuspendLayout();
             // 
@@ -5079,9 +5059,7 @@
             // 
             // tabPageUser
             // 
-            this.tabPageUser.Controls.Add(this.groupBox29);
-            this.tabPageUser.Controls.Add(this.gridControlUsers);
-            this.tabPageUser.Controls.Add(this.groupBox30);
+            this.tabPageUser.Controls.Add(this.usersControl1);
             this.tabPageUser.ImageKey = "accounts.png";
             this.tabPageUser.Location = new System.Drawing.Point(4, 39);
             this.tabPageUser.Name = "tabPageUser";
@@ -5089,204 +5067,6 @@
             this.tabPageUser.TabIndex = 7;
             this.tabPageUser.Text = "Users";
             this.tabPageUser.UseVisualStyleBackColor = true;
-            // 
-            // groupBox29
-            // 
-            this.groupBox29.Controls.Add(this.label88);
-            this.groupBox29.Location = new System.Drawing.Point(7, 14);
-            this.groupBox29.Name = "groupBox29";
-            this.groupBox29.Size = new System.Drawing.Size(379, 148);
-            this.groupBox29.TabIndex = 9;
-            this.groupBox29.TabStop = false;
-            this.groupBox29.Text = "Total User";
-            // 
-            // label88
-            // 
-            this.label88.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label88.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label88.Location = new System.Drawing.Point(3, 29);
-            this.label88.Name = "label88";
-            this.label88.Size = new System.Drawing.Size(373, 116);
-            this.label88.TabIndex = 0;
-            this.label88.Text = "00";
-            this.label88.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // gridControlUsers
-            // 
-            this.gridControlUsers.Location = new System.Drawing.Point(392, 3);
-            this.gridControlUsers.MainView = this.gridViewUsers;
-            this.gridControlUsers.Name = "gridControlUsers";
-            this.gridControlUsers.Size = new System.Drawing.Size(877, 616);
-            this.gridControlUsers.TabIndex = 8;
-            this.gridControlUsers.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
-            this.gridViewUsers});
-            // 
-            // gridViewUsers
-            // 
-            this.gridViewUsers.GridControl = this.gridControlUsers;
-            this.gridViewUsers.Name = "gridViewUsers";
-            // 
-            // groupBox30
-            // 
-            this.groupBox30.Controls.Add(this.textBox24);
-            this.groupBox30.Controls.Add(this.label96);
-            this.groupBox30.Controls.Add(this.textBox23);
-            this.groupBox30.Controls.Add(this.label95);
-            this.groupBox30.Controls.Add(this.textBox21);
-            this.groupBox30.Controls.Add(this.label93);
-            this.groupBox30.Controls.Add(this.button15);
-            this.groupBox30.Controls.Add(this.checkBox2);
-            this.groupBox30.Controls.Add(this.textBox19);
-            this.groupBox30.Controls.Add(this.label89);
-            this.groupBox30.Controls.Add(this.textBox20);
-            this.groupBox30.Controls.Add(this.label91);
-            this.groupBox30.Location = new System.Drawing.Point(7, 168);
-            this.groupBox30.Name = "groupBox30";
-            this.groupBox30.Size = new System.Drawing.Size(379, 279);
-            this.groupBox30.TabIndex = 7;
-            this.groupBox30.TabStop = false;
-            this.groupBox30.Text = "Add User";
-            // 
-            // textBox24
-            // 
-            this.textBox24.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox24.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox24.Location = new System.Drawing.Point(135, 170);
-            this.textBox24.Name = "textBox24";
-            this.textBox24.Size = new System.Drawing.Size(238, 26);
-            this.textBox24.TabIndex = 34;
-            // 
-            // label96
-            // 
-            this.label96.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label96.AutoSize = true;
-            this.label96.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label96.Location = new System.Drawing.Point(9, 173);
-            this.label96.Name = "label96";
-            this.label96.Size = new System.Drawing.Size(120, 20);
-            this.label96.TabIndex = 33;
-            this.label96.Text = "Conf. Password";
-            // 
-            // textBox23
-            // 
-            this.textBox23.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox23.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox23.Location = new System.Drawing.Point(135, 138);
-            this.textBox23.Name = "textBox23";
-            this.textBox23.Size = new System.Drawing.Size(238, 26);
-            this.textBox23.TabIndex = 32;
-            // 
-            // label95
-            // 
-            this.label95.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label95.AutoSize = true;
-            this.label95.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label95.Location = new System.Drawing.Point(51, 141);
-            this.label95.Name = "label95";
-            this.label95.Size = new System.Drawing.Size(78, 20);
-            this.label95.TabIndex = 31;
-            this.label95.Text = "Password";
-            // 
-            // textBox21
-            // 
-            this.textBox21.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox21.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox21.Location = new System.Drawing.Point(135, 106);
-            this.textBox21.Name = "textBox21";
-            this.textBox21.Size = new System.Drawing.Size(238, 26);
-            this.textBox21.TabIndex = 30;
-            // 
-            // label93
-            // 
-            this.label93.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label93.AutoSize = true;
-            this.label93.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label93.Location = new System.Drawing.Point(46, 109);
-            this.label93.Name = "label93";
-            this.label93.Size = new System.Drawing.Size(83, 20);
-            this.label93.TabIndex = 29;
-            this.label93.Text = "Username";
-            // 
-            // button15
-            // 
-            this.button15.Location = new System.Drawing.Point(268, 237);
-            this.button15.Name = "button15";
-            this.button15.Size = new System.Drawing.Size(105, 35);
-            this.button15.TabIndex = 28;
-            this.button15.Text = "Add";
-            this.button15.UseVisualStyleBackColor = true;
-            // 
-            // checkBox2
-            // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Checked = true;
-            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBox2.Location = new System.Drawing.Point(135, 202);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(82, 29);
-            this.checkBox2.TabIndex = 27;
-            this.checkBox2.Text = "Active";
-            this.checkBox2.UseVisualStyleBackColor = true;
-            // 
-            // textBox19
-            // 
-            this.textBox19.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox19.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox19.Location = new System.Drawing.Point(135, 74);
-            this.textBox19.Name = "textBox19";
-            this.textBox19.Size = new System.Drawing.Size(238, 26);
-            this.textBox19.TabIndex = 26;
-            // 
-            // label89
-            // 
-            this.label89.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label89.AutoSize = true;
-            this.label89.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label89.Location = new System.Drawing.Point(81, 77);
-            this.label89.Name = "label89";
-            this.label89.Size = new System.Drawing.Size(48, 20);
-            this.label89.TabIndex = 25;
-            this.label89.Text = "Email";
-            // 
-            // textBox20
-            // 
-            this.textBox20.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox20.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox20.Location = new System.Drawing.Point(135, 42);
-            this.textBox20.Name = "textBox20";
-            this.textBox20.Size = new System.Drawing.Size(238, 26);
-            this.textBox20.TabIndex = 24;
-            // 
-            // label91
-            // 
-            this.label91.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label91.AutoSize = true;
-            this.label91.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label91.Location = new System.Drawing.Point(78, 45);
-            this.label91.Name = "label91";
-            this.label91.Size = new System.Drawing.Size(51, 20);
-            this.label91.TabIndex = 23;
-            this.label91.Text = "Name";
             // 
             // imageListTab
             // 
@@ -5306,6 +5086,15 @@
             this.imageListTab.Images.SetKeyName(11, "processing.png");
             this.imageListTab.Images.SetKeyName(12, "hammer.png");
             this.imageListTab.Images.SetKeyName(13, "builder.png");
+            // 
+            // usersControl1
+            // 
+            this.usersControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.usersControl1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usersControl1.Location = new System.Drawing.Point(0, 0);
+            this.usersControl1.Name = "usersControl1";
+            this.usersControl1.Size = new System.Drawing.Size(1276, 622);
+            this.usersControl1.TabIndex = 0;
             // 
             // MainForm
             // 
@@ -5434,11 +5223,6 @@
             this.groupBox32.ResumeLayout(false);
             this.groupBox32.PerformLayout();
             this.tabPageUser.ResumeLayout(false);
-            this.groupBox29.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.gridControlUsers)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.gridViewUsers)).EndInit();
-            this.groupBox30.ResumeLayout(false);
-            this.groupBox30.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSourcePurchaseItem)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -5838,23 +5622,6 @@
         private System.Windows.Forms.Label label86;
         private System.Windows.Forms.TextBox textBox17;
         private System.Windows.Forms.Label label81;
-        private System.Windows.Forms.GroupBox groupBox29;
-        private System.Windows.Forms.Label label88;
-        private DevExpress.XtraGrid.GridControl gridControlUsers;
-        private DevExpress.XtraGrid.Views.Grid.GridView gridViewUsers;
-        private System.Windows.Forms.GroupBox groupBox30;
-        private System.Windows.Forms.TextBox textBox24;
-        private System.Windows.Forms.Label label96;
-        private System.Windows.Forms.TextBox textBox23;
-        private System.Windows.Forms.Label label95;
-        private System.Windows.Forms.TextBox textBox21;
-        private System.Windows.Forms.Label label93;
-        private System.Windows.Forms.Button button15;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.TextBox textBox19;
-        private System.Windows.Forms.Label label89;
-        private System.Windows.Forms.TextBox textBox20;
-        private System.Windows.Forms.Label label91;
         private DevExpress.XtraGrid.GridControl gridControlWorks;
         private DevExpress.XtraGrid.Views.Grid.GridView gridViewWorks;
         private System.Windows.Forms.GroupBox groupBox32;
@@ -5867,6 +5634,7 @@
         private System.Windows.Forms.TextBox textBox26;
         private System.Windows.Forms.Label label98;
         private System.Windows.Forms.Label label99;
+        private ViewControls.UsersControl usersControl1;
     }
 }
 
