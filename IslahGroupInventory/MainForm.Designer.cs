ef583b01e94437a7f536e1fefc06db2104756991
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2018-03-20 10:35:57 +0600

    WorksControl add

--- IslahGroupInventory/MainForm.Designer.cs
+++ IslahGroupInventory/MainForm.Designer.cs
@@ -61,19 +61,8 @@
             this.tabPageBarcode = new System.Windows.Forms.TabPage();
             this.barcodeControl1 = new IslahGroupInventory.ViewControls.BarcodeControl();
             this.tabPageWorker = new System.Windows.Forms.TabPage();
+            this.workersControl1 = new IslahGroupInventory.ViewControls.WorkersControl();
             this.tabPageWorks = new System.Windows.Forms.TabPage();
-            this.gridControlWorks = new DevExpress.XtraGrid.GridControl();
-            this.gridViewWorks = new DevExpress.XtraGrid.Views.Grid.GridView();
-            this.groupBox32 = new System.Windows.Forms.GroupBox();
-            this.comboBox6 = new System.Windows.Forms.ComboBox();
-            this.label100 = new System.Windows.Forms.Label();
-            this.comboBox5 = new System.Windows.Forms.ComboBox();
-            this.dateTimePicker5 = new System.Windows.Forms.DateTimePicker();
-            this.label97 = new System.Windows.Forms.Label();
-            this.button16 = new System.Windows.Forms.Button();
-            this.textBox26 = new System.Windows.Forms.TextBox();
-            this.label98 = new System.Windows.Forms.Label();
-            this.label99 = new System.Windows.Forms.Label();
             this.tabPageUser = new System.Windows.Forms.TabPage();
             this.usersControl1 = new IslahGroupInventory.ViewControls.UsersControl();
             this.imageListTab = new System.Windows.Forms.ImageList(this.components);
@@ -83,7 +72,7 @@
             this.customersBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.rawProductsBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.bindingSourcePurchaseItem = new System.Windows.Forms.BindingSource(this.components);
-            this.workersControl1 = new IslahGroupInventory.ViewControls.WorkersControl();
+            this.worksControl1 = new IslahGroupInventory.ViewControls.WorksControl();
             this.menuStrip1.SuspendLayout();
             this.statusStrip1.SuspendLayout();
             this.tabControlMain.SuspendLayout();
@@ -99,9 +88,6 @@
             this.tabPageBarcode.SuspendLayout();
             this.tabPageWorker.SuspendLayout();
             this.tabPageWorks.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.gridControlWorks)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.gridViewWorks)).BeginInit();
-            this.groupBox32.SuspendLayout();
             this.tabPageUser.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.productCategoryBindingSource)).BeginInit();
@@ -411,10 +397,17 @@
             this.tabPageWorker.Text = "Workers";
             this.tabPageWorker.UseVisualStyleBackColor = true;
             // 
+            // workersControl1
+            // 
+            this.workersControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.workersControl1.Location = new System.Drawing.Point(0, 0);
+            this.workersControl1.Name = "workersControl1";
+            this.workersControl1.Size = new System.Drawing.Size(1276, 622);
+            this.workersControl1.TabIndex = 0;
+            // 
             // tabPageWorks
             // 
-            this.tabPageWorks.Controls.Add(this.gridControlWorks);
-            this.tabPageWorks.Controls.Add(this.groupBox32);
+            this.tabPageWorks.Controls.Add(this.worksControl1);
             this.tabPageWorks.ImageKey = "hammer.png";
             this.tabPageWorks.Location = new System.Drawing.Point(4, 39);
             this.tabPageWorks.Name = "tabPageWorks";
@@ -422,136 +415,6 @@
             this.tabPageWorks.TabIndex = 12;
             this.tabPageWorks.Text = "Works";
             this.tabPageWorks.UseVisualStyleBackColor = true;
-            // 
-            // gridControlWorks
-            // 
-            this.gridControlWorks.Location = new System.Drawing.Point(373, 3);
-            this.gridControlWorks.MainView = this.gridViewWorks;
-            this.gridControlWorks.Name = "gridControlWorks";
-            this.gridControlWorks.Size = new System.Drawing.Size(896, 616);
-            this.gridControlWorks.TabIndex = 8;
-            this.gridControlWorks.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
-            this.gridViewWorks});
-            // 
-            // gridViewWorks
-            // 
-            this.gridViewWorks.GridControl = this.gridControlWorks;
-            this.gridViewWorks.Name = "gridViewWorks";
-            // 
-            // groupBox32
-            // 
-            this.groupBox32.Controls.Add(this.comboBox6);
-            this.groupBox32.Controls.Add(this.label100);
-            this.groupBox32.Controls.Add(this.comboBox5);
-            this.groupBox32.Controls.Add(this.dateTimePicker5);
-            this.groupBox32.Controls.Add(this.label97);
-            this.groupBox32.Controls.Add(this.button16);
-            this.groupBox32.Controls.Add(this.textBox26);
-            this.groupBox32.Controls.Add(this.label98);
-            this.groupBox32.Controls.Add(this.label99);
-            this.groupBox32.Location = new System.Drawing.Point(8, 12);
-            this.groupBox32.Name = "groupBox32";
-            this.groupBox32.Size = new System.Drawing.Size(359, 231);
-            this.groupBox32.TabIndex = 7;
-            this.groupBox32.TabStop = false;
-            this.groupBox32.Text = "Add Work";
-            // 
-            // comboBox6
-            // 
-            this.comboBox6.FormattingEnabled = true;
-            this.comboBox6.Location = new System.Drawing.Point(96, 77);
-            this.comboBox6.Name = "comboBox6";
-            this.comboBox6.Size = new System.Drawing.Size(253, 33);
-            this.comboBox6.TabIndex = 42;
-            // 
-            // label100
-            // 
-            this.label100.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label100.AutoSize = true;
-            this.label100.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label100.Location = new System.Drawing.Point(6, 84);
-            this.label100.Name = "label100";
-            this.label100.Size = new System.Drawing.Size(84, 20);
-            this.label100.TabIndex = 41;
-            this.label100.Text = "Work Type";
-            // 
-            // comboBox5
-            // 
-            this.comboBox5.FormattingEnabled = true;
-            this.comboBox5.Location = new System.Drawing.Point(96, 38);
-            this.comboBox5.Name = "comboBox5";
-            this.comboBox5.Size = new System.Drawing.Size(253, 33);
-            this.comboBox5.TabIndex = 40;
-            // 
-            // dateTimePicker5
-            // 
-            this.dateTimePicker5.CustomFormat = "";
-            this.dateTimePicker5.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker5.Location = new System.Drawing.Point(95, 116);
-            this.dateTimePicker5.Name = "dateTimePicker5";
-            this.dateTimePicker5.Size = new System.Drawing.Size(254, 33);
-            this.dateTimePicker5.TabIndex = 39;
-            // 
-            // label97
-            // 
-            this.label97.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label97.AutoSize = true;
-            this.label97.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label97.Location = new System.Drawing.Point(46, 126);
-            this.label97.Name = "label97";
-            this.label97.Size = new System.Drawing.Size(44, 20);
-            this.label97.TabIndex = 38;
-            this.label97.Text = "Date";
-            // 
-            // button16
-            // 
-            this.button16.Location = new System.Drawing.Point(244, 187);
-            this.button16.Name = "button16";
-            this.button16.Size = new System.Drawing.Size(105, 35);
-            this.button16.TabIndex = 28;
-            this.button16.Text = "Add";
-            this.button16.UseVisualStyleBackColor = true;
-            // 
-            // textBox26
-            // 
-            this.textBox26.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox26.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox26.Location = new System.Drawing.Point(95, 155);
-            this.textBox26.Name = "textBox26";
-            this.textBox26.Size = new System.Drawing.Size(254, 26);
-            this.textBox26.TabIndex = 26;
-            // 
-            // label98
-            // 
-            this.label98.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label98.AutoSize = true;
-            this.label98.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label98.Location = new System.Drawing.Point(22, 158);
-            this.label98.Name = "label98";
-            this.label98.Size = new System.Drawing.Size(68, 20);
-            this.label98.TabIndex = 25;
-            this.label98.Text = "Quantity";
-            // 
-            // label99
-            // 
-            this.label99.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label99.AutoSize = true;
-            this.label99.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label99.Location = new System.Drawing.Point(30, 45);
-            this.label99.Name = "label99";
-            this.label99.Size = new System.Drawing.Size(60, 20);
-            this.label99.TabIndex = 23;
-            this.label99.Text = "Worker";
             // 
             // tabPageUser
             // 
@@ -608,13 +471,13 @@
             // 
             this.rawProductsBindingSource.DataSource = typeof(IslahGroupInventory.RawProduct);
             // 
-            // workersControl1
+            // worksControl1
             // 
-            this.workersControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.workersControl1.Location = new System.Drawing.Point(0, 0);
-            this.workersControl1.Name = "workersControl1";
-            this.workersControl1.Size = new System.Drawing.Size(1276, 622);
-            this.workersControl1.TabIndex = 0;
+            this.worksControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.worksControl1.Location = new System.Drawing.Point(0, 0);
+            this.worksControl1.Name = "worksControl1";
+            this.worksControl1.Size = new System.Drawing.Size(1276, 622);
+            this.worksControl1.TabIndex = 0;
             // 
             // MainForm
             // 
@@ -647,10 +510,6 @@
             this.tabPageBarcode.ResumeLayout(false);
             this.tabPageWorker.ResumeLayout(false);
             this.tabPageWorks.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.gridControlWorks)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.gridViewWorks)).EndInit();
-            this.groupBox32.ResumeLayout(false);
-            this.groupBox32.PerformLayout();
             this.tabPageUser.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.productCategoryBindingSource)).EndInit();
@@ -697,18 +556,6 @@
         private DevExpress.XtraGrid.Columns.GridColumn PPId;
         private System.Windows.Forms.TabPage tabPageWorker;
         private System.Windows.Forms.TabPage tabPageWorks;
-        private DevExpress.XtraGrid.GridControl gridControlWorks;
-        private DevExpress.XtraGrid.Views.Grid.GridView gridViewWorks;
-        private System.Windows.Forms.GroupBox groupBox32;
-        private System.Windows.Forms.ComboBox comboBox6;
-        private System.Windows.Forms.Label label100;
-        private System.Windows.Forms.ComboBox comboBox5;
-        private System.Windows.Forms.DateTimePicker dateTimePicker5;
-        private System.Windows.Forms.Label label97;
-        private System.Windows.Forms.Button button16;
-        private System.Windows.Forms.TextBox textBox26;
-        private System.Windows.Forms.Label label98;
-        private System.Windows.Forms.Label label99;
         private ViewControls.UsersControl usersControl1;
         private ViewControls.RawMaterialControl rawMaterialControl1;
         private ViewControls.ProcessingControl processingControl1;
@@ -721,6 +568,7 @@
         private ViewControls.SuppliersControl suppliersControl1;
         private ViewControls.BarcodeControl barcodeControl1;
         private ViewControls.WorkersControl workersControl1;
+        private ViewControls.WorksControl worksControl1;
     }
 }
 
---
+++ IslahGroupInventory/ViewControls/WorksControl.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace IslahGroupInventory.ViewControls
+{
+    partial class WorksControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridControlWorks = new DevExpress.XtraGrid.GridControl();
+            this.gridViewWorks = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.groupBox32 = new System.Windows.Forms.GroupBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.label100 = new System.Windows.Forms.Label();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker5 = new System.Windows.Forms.DateTimePicker();
+            this.label97 = new System.Windows.Forms.Label();
+            this.button16 = new System.Windows.Forms.Button();
+            this.textBox26 = new System.Windows.Forms.TextBox();
+            this.label98 = new System.Windows.Forms.Label();
+            this.label99 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControlWorks)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewWorks)).BeginInit();
+            this.groupBox32.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gridControlWorks
+            // 
+            this.gridControlWorks.Location = new System.Drawing.Point(373, 3);
+            this.gridControlWorks.MainView = this.gridViewWorks;
+            this.gridControlWorks.Name = "gridControlWorks";
+            this.gridControlWorks.Size = new System.Drawing.Size(900, 616);
+            this.gridControlWorks.TabIndex = 10;
+            this.gridControlWorks.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridViewWorks});
+            // 
+            // gridViewWorks
+            // 
+            this.gridViewWorks.GridControl = this.gridControlWorks;
+            this.gridViewWorks.Name = "gridViewWorks";
+            // 
+            // groupBox32
+            // 
+            this.groupBox32.Controls.Add(this.comboBox6);
+            this.groupBox32.Controls.Add(this.label100);
+            this.groupBox32.Controls.Add(this.comboBox5);
+            this.groupBox32.Controls.Add(this.dateTimePicker5);
+            this.groupBox32.Controls.Add(this.label97);
+            this.groupBox32.Controls.Add(this.button16);
+            this.groupBox32.Controls.Add(this.textBox26);
+            this.groupBox32.Controls.Add(this.label98);
+            this.groupBox32.Controls.Add(this.label99);
+            this.groupBox32.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox32.Location = new System.Drawing.Point(3, 3);
+            this.groupBox32.Name = "groupBox32";
+            this.groupBox32.Size = new System.Drawing.Size(364, 231);
+            this.groupBox32.TabIndex = 9;
+            this.groupBox32.TabStop = false;
+            this.groupBox32.Text = "Add Work";
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Location = new System.Drawing.Point(96, 77);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(258, 32);
+            this.comboBox6.TabIndex = 42;
+            // 
+            // label100
+            // 
+            this.label100.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label100.AutoSize = true;
+            this.label100.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label100.Location = new System.Drawing.Point(6, 83);
+            this.label100.Name = "label100";
+            this.label100.Size = new System.Drawing.Size(84, 20);
+            this.label100.TabIndex = 41;
+            this.label100.Text = "Work Type";
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(96, 38);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(258, 32);
+            this.comboBox5.TabIndex = 40;
+            // 
+            // dateTimePicker5
+            // 
+            this.dateTimePicker5.CustomFormat = "";
+            this.dateTimePicker5.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker5.Location = new System.Drawing.Point(95, 116);
+            this.dateTimePicker5.Name = "dateTimePicker5";
+            this.dateTimePicker5.Size = new System.Drawing.Size(259, 29);
+            this.dateTimePicker5.TabIndex = 39;
+            // 
+            // label97
+            // 
+            this.label97.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label97.AutoSize = true;
+            this.label97.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label97.Location = new System.Drawing.Point(46, 123);
+            this.label97.Name = "label97";
+            this.label97.Size = new System.Drawing.Size(44, 20);
+            this.label97.TabIndex = 38;
+            this.label97.Text = "Date";
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(244, 187);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(105, 35);
+            this.button16.TabIndex = 28;
+            this.button16.Text = "Add";
+            this.button16.UseVisualStyleBackColor = true;
+            // 
+            // textBox26
+            // 
+            this.textBox26.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox26.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox26.Location = new System.Drawing.Point(95, 155);
+            this.textBox26.Name = "textBox26";
+            this.textBox26.Size = new System.Drawing.Size(259, 26);
+            this.textBox26.TabIndex = 26;
+            // 
+            // label98
+            // 
+            this.label98.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label98.AutoSize = true;
+            this.label98.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label98.Location = new System.Drawing.Point(22, 158);
+            this.label98.Name = "label98";
+            this.label98.Size = new System.Drawing.Size(68, 20);
+            this.label98.TabIndex = 25;
+            this.label98.Text = "Quantity";
+            // 
+            // label99
+            // 
+            this.label99.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label99.AutoSize = true;
+            this.label99.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label99.Location = new System.Drawing.Point(30, 44);
+            this.label99.Name = "label99";
+            this.label99.Size = new System.Drawing.Size(60, 20);
+            this.label99.TabIndex = 23;
+            this.label99.Text = "Worker";
+            // 
+            // WorksControl
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.Controls.Add(this.gridControlWorks);
+            this.Controls.Add(this.groupBox32);
+            this.Name = "WorksControl";
+            this.Size = new System.Drawing.Size(1276, 622);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControlWorks)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewWorks)).EndInit();
+            this.groupBox32.ResumeLayout(false);
+            this.groupBox32.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControlWorks;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridViewWorks;
+        private System.Windows.Forms.GroupBox groupBox32;
+        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.Label label100;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker5;
+        private System.Windows.Forms.Label label97;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.TextBox textBox26;
+        private System.Windows.Forms.Label label98;
+        private System.Windows.Forms.Label label99;
+    }
+}
